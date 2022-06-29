6f5a9915422899a5ab9c88625151592fd124cdc8
boniich <ezequieldbo22@hotmail.com>
2022-06-29 08:24:27 -0300

    feat: add buttons and turn system

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [intervalStatus, setIntervalStatus] = useState(0);
+  const [start, setStart] = useState(false);
+  const [turn, setTurn] = useState(0);
+
+  const startGame = () => {
+    setStart(true);
+  };
+
+  const stopGame = () =>{
+    setStart(false);
+  }
+
+  const reset = () =>{
+    setStart(false);
+    setTurn(0);
+  }
+
+  useEffect(() =>{
+    let intervalId = null;
+    if(start){
+      intervalId = setInterval(() =>{
+        setTurn(turn => turn +=1);
+      },1000);
+
+      setIntervalStatus(intervalId);
+      
+    }else{
+      clearInterval(intervalStatus);
+    }
+  }, [start]);
+
+
   return (
     <section className="game-section">
       {/* contiene los botones y el contador */}
       <div className="nav-bar">
         <div className="button-container">
-          <button>Iniciar</button>
-          <button>Detener</button>
-          <button>Reiniciar</button>
+          <button onClick={startGame}>Iniciar</button>
+          <button onClick={stopGame}>Detener</button>
+          <button onClick={reset}>Reiniciar</button>
         </div>
         <div>
-          <p>Generacion: </p>
+          <p>Generacion: {turn}</p>
         </div>
       </div>
       {/* el tablero */}
