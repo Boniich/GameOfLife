a75a2ecb58687fbc4d14548eaeb3ce09d74330d2
boniich <ezequieldbo22@hotmail.com>
2022-06-30 12:18:06 -0300

    feat: add a dinamic board

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+
+let boardHeigth = 50;
+let boardWidth = 30;
+
+const printBoard = () => {
+  const board = [];
+
+  for (let e = 0; e < boardHeigth; e++) {
+    const row = [];
+    for (let j = 0; j < boardWidth; j++) {
+      row.push(0);
+    }
+    board.push(row);
+  }
+
+  return board;
+};
+
 function App() {
   const [intervalStatus, setIntervalStatus] = useState(0);
+  // const [boardHeigth, setBoardHeigth] = useState(30);
+  const [board, setBoard] = useState();
   const [start, setStart] = useState(false);
   const [turn, setTurn] = useState(0);
+
+  useEffect(() => {
+    setBoard(printBoard());
+  }, []);
 
   const startGame = () => {
     setStart(true);
   };
 
-  const stopGame = () =>{
+  const stopGame = () => {
     setStart(false);
-  }
+  };
 
-  const reset = () =>{
+  const reset = () => {
     setStart(false);
     setTurn(0);
-  }
+  };
 
-  useEffect(() =>{
+  useEffect(() => {
     let intervalId = null;
-    if(start){
-      intervalId = setInterval(() =>{
-        setTurn(turn => turn +=1);
-      },1000);
+    if (start) {
+      intervalId = setInterval(() => {
+        setTurn((turn) => (turn += 1));
+      }, 1000);
 
       setIntervalStatus(intervalId);
-      
-    }else{
+    } else {
       clearInterval(intervalStatus);
     }
+    // console.log(cell);
   }, [start]);
-
 
   return (
     <section className="game-section">
@@ -47,8 +70,16 @@ function App() {
           <p>Generacion: {turn}</p>
         </div>
       </div>
-      {/* el tablero */}
-      <div></div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${boardHeigth}, 25px)`,
+          width: "fit-content",
+          margin: "0 auto",
+        }}
+      >
+        {board && board.map((rows, i) => rows.map((col, k) => <div className="circle"></div>))}
+      </div>
     </section>
   );
 }
