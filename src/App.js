1231a1cb2044564dfbb7177ab3ccceeee0c01c08
boniich <ezequieldbo22@hotmail.com>
2022-07-01 20:11:51 -0300

    feat: add a modal for havethe configurations values

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useInterval } from "./hooks/useInterval";
 import { printBoard } from "./services/printBoard/printBoard";
+import Popup from "reactjs-popup";
 
 // uso estas posiciones para determinar las celulas vecinas que pueden estar vivas o muertas
 const positions = [
@@ -21,51 +22,59 @@ function App() {
   const [turn, setTurn] = useState(0);
   const [boardRows, setBoardRows] = useState(30);
   const [boardCols, setBoardCols] = useState(50);
-  const [delay,setDelay] = useState(1000);
+  const [delay, setDelay] = useState(1000);
 
   // creamos una referencia del valor de inicio y pausa para poder actualizar
   // el valor durante la simulacion
   const runningRef = useRef(start);
   runningRef.current = start;
 
-  const runSimulation = useCallback((board) => {
-    if (!runningRef.current) {
-      return;
-    }
+  const runSimulation = useCallback(
+    (board) => {
+      if (!runningRef.current) {
+        return;
+      }
 
-    setTurn((turn) => (turn += 1));
-    let boardCopy = JSON.parse(JSON.stringify(board));
-    for (let i = 0; i < boardRows; i++) {
-      for (let j = 0; j < boardCols; j++) {
-        let neighbors = 0;
-        positions.forEach(([x, y]) => {
-          const newI = i + x;
-          const newJ = j + y;
+      setTurn((turn) => (turn += 1));
+      let boardCopy = JSON.parse(JSON.stringify(board));
+      for (let i = 0; i < boardRows; i++) {
+        for (let j = 0; j < boardCols; j++) {
+          let neighbors = 0;
+          positions.forEach(([x, y]) => {
+            const newI = i + x;
+            const newJ = j + y;
 
-          if (newI >= 0 && newI < boardRows && newJ >= 0 && newJ < boardCols) {
-            neighbors += board[newI][newJ];
+            if (
+              newI >= 0 &&
+              newI < boardRows &&
+              newJ >= 0 &&
+              newJ < boardCols
+            ) {
+              neighbors += board[newI][newJ];
+            }
+          });
+          // si tiene menos de dos celulas vecinas vivias o mas de 3 muere la celula
+          if (neighbors < 2 || neighbors > 3) {
+            boardCopy[i][j] = 0;
+            // si hay tres celula vivias y una muerta, esta celula muerta, "vivira" al siguiente turno
+          } else if (boardCopy[i][j] === 0 && neighbors === 3) {
+            boardCopy[i][j] = 1;
           }
-        });
-        // si tiene menos de dos celulas vecinas vivias o mas de 3 muere la celula
-        if (neighbors < 2 || neighbors > 3) {
-          boardCopy[i][j] = 0;
-          // si hay tres celula vivias y una muerta, esta celula muerta, "vivira" al siguiente turno
-        } else if (boardCopy[i][j] === 0 && neighbors === 3) {
-          boardCopy[i][j] = 1;
         }
       }
-    }
 
-    setBoard(boardCopy);
-  }, [boardRows,boardCols]);
+      setBoard(boardCopy);
+    },
+    [boardRows, boardCols]
+  );
 
   // imprimimos el teclado cuando se inicia la pagina
   useEffect(() => {
-    const board = printBoard(boardRows,boardCols)
-    console.log("row:",boardRows);
+    const board = printBoard(boardRows, boardCols);
+    console.log("row:", boardRows);
     setBoard(board);
     console.log(board);
-  }, [boardRows,boardCols]);
+  }, [boardRows, boardCols]);
 
   const startGame = () => {
     setStart(true);
@@ -83,7 +92,7 @@ function App() {
     setStart(false);
     setTurn(0);
     // reiniciamos el table usando tablero inicial, ya que todos los valores son de 0
-    setBoard(printBoard(boardRows,boardCols));
+    setBoard(printBoard(boardRows, boardCols));
   };
 
   // este es un custom hooks que encontre que resuelve un problema entre el setInterval y react
@@ -95,6 +104,14 @@ function App() {
   console.log(boardRows);
   console.log(boardCols);
 
+  const validate = () => {
+    if (start) {
+      alert("no podes cambiar la configuracion con la simulacion iniciada");
+    } else {
+      return false;
+    }
+  };
+
   return (
     <section className="game-section">
       <div className="nav-bar">
@@ -102,9 +119,65 @@ function App() {
           <button onClick={startGame}>Iniciar</button>
           <button onClick={stopGame}>Detener</button>
           <button onClick={reset}>Reiniciar</button>
-          <input type="number"  value={boardRows} onChange={e => setBoardRows(e.target.value)}/>
-          <input type="number"  value={boardCols} onChange={e => setBoardCols(e.target.value)}/>
-          <input type="number"  value={delay} onChange={e => setDelay(e.target.value)}/>
+          <Popup trigger={<button>Configuracion</button>} modal closeOnDocumentClick={false} closeOnEscape={false}>
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Configuracion</div>
+                <div className="content">
+                  <div className="inputs-container">
+                    <label className="labels-inputs">Filas</label>
+                    <input
+                      type="range"
+                      value={boardRows}
+                      onChange={(e) =>
+                        validate() === false && setBoardRows(e.target.value)
+                      }
+                    />
+                    <p className="show-data-input">{boardRows}</p>
+                  </div>
+
+                  <div className="inputs-container">
+                    <label className="labels-inputs">Columnas</label>
+                    <input
+                      type="range"
+                      value={boardCols}
+                      onChange={(e) =>
+                        validate() === false && setBoardCols(e.target.value)
+                      }
+                    />
+                    <p className="show-data-input">{boardCols}</p>
+                  </div>
+
+                  <div className="inputs-container">
+                    <label className="labels-inputs">Delay</label>
+                    <input
+                      type="range"
+                      max={10000}
+                      value={delay}
+                      onChange={(e) =>
+                        validate() === false && setDelay(e.target.value)
+                      }
+                    />
+                    <p className="show-data-input">{delay}</p>
+                  </div>
+                </div>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log("modal closed ");
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </div>
         <div>
           <p>Generacion: {turn}</p>
