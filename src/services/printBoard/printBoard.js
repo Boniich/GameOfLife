223d739d78b5e36a88c82574780f04b5c89eadea
boniich <ezequieldbo22@hotmail.com>
2022-07-02 08:43:55 -0300

    refactor: now this function recive just one state that inside use his props

--- src/services/printBoard/printBoard.js
+++ src/services/printBoard/printBoard.js
@@ -1,9 +1,9 @@
-export const printBoard = (boardRows,boardCols) => {
+export const printBoard = (config) => {
     const row = [];
-    for (let j = 0; j < boardRows; j++) {
+    for (let j = 0; j < config.boardRows; j++) {
       // creamos una copia del array con las columnas y los agregamos en el array de row
       // agregamos un 0, es decir una celula muerta por cada columna, dentro del row
-      row.push(Array.from(Array(boardCols), () => 0));
+      row.push(Array.from(Array(config.boardCols), () => 0));
     }
     return row;
   };
