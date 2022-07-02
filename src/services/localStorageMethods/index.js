c7364b548608fcb6eb56045836506a0c25171c9a
boniich <ezequieldbo22@hotmail.com>
2022-07-02 15:08:43 -0300

    feat: add method to delete all saved games

--- src/services/localStorageMethods/index.js
+++ src/services/localStorageMethods/index.js
@@ -1,16 +1,17 @@
+export const saveGameInStorage = (dataToSave) => {
+  // al momento de guardar el array con sus elementos en el localstorage
+  // lo convertimos a string para que se guarde correctamente
+  localStorage.setItem("save", JSON.stringify(dataToSave));
+};
 
-export const saveGameInStorage = (dataToSave) =>{
-    // al momento de guardar el array con sus elementos en el localstorage
-    // lo convertimos a string para que se guarde correctamente
-    localStorage.setItem(
-        "save",
-        JSON.stringify(dataToSave)
-      );
-} 
+export const loadSavedGameFromStorage = () => {
+  // cuando traemos el array del localStorage lo convertimos
+  // a javascript para poder utilizarlo y renderizarlo
+  const savedGame = JSON.parse(localStorage.getItem("save"));
+  return savedGame;
+};
 
-export const loadSavedGameFromStorage = () =>{
-    // cuando traemos el array del localStorage lo convertimos
-    // a javascript para poder utilizarlo y renderizarlo
-    const savedGame = JSON.parse(localStorage.getItem("save"));
-    return savedGame;
-}
+export const deleteAllSavedGames = (setSavedGame) => {
+  localStorage.removeItem("save");
+  setSavedGame([]);
+};
