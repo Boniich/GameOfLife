46ed53f7420e7e8a278a6aaac2913dd1aedb0c0d
boniich <ezequieldbo22@hotmail.com>
2022-07-01 23:40:57 -0300

    feat: modularize localstorage methods

---
+++ src/services/localStorageMethods/index.js
@@ -0,0 +1,16 @@
+
+export const savedGameInStorage = (dataToSave) =>{
+    // al momento de guardar el array con sus elementos en el localstorage
+    // lo convertimos a string para que se guarde correctamente
+    localStorage.setItem(
+        "save",
+        JSON.stringify([dataToSave])
+      );
+} 
+
+export const loadGameFromStorage = () =>{
+    // cuando traemos el array del localStorage lo convertimos
+    // a javascript para poder utilizarlo y renderizarlo
+    const savedGame = JSON.parse(localStorage.getItem("save"));
+    return savedGame;
+}
