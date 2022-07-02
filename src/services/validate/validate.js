4b4186d69998e47f10da00ce6cad13c094bbeb8d
boniich <ezequieldbo22@hotmail.com>
2022-07-02 08:59:27 -0300

    feat: delete function cause is not will used anymore

--- src/services/validate/validate.js
+++
@@ -1,9 +0,0 @@
-// validate chequea si la simulacion esta siendo ejecutada para no permitir cambios en la configuracion
-
-export const validate = (start) => {
-  if (start) {
-    alert("no podes cambiar la configuracion con la simulacion iniciada");
-  } else {
-    return false;
-  }
-};
