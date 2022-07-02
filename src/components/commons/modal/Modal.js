869fdf0af4a53bfb8638445677d2f49431801a38
boniich <ezequieldbo22@hotmail.com>
2022-07-02 15:08:20 -0300

    feat: add re usable modal

---
+++ src/components/commons/modal/Modal.js
@@ -0,0 +1,37 @@
+import Popup from "reactjs-popup";
+
+export const Modal = ({children,trigger,textHeader,extraButton}) =>{
+
+    return(
+        <Popup
+        trigger={trigger}
+        modal
+        closeOnDocumentClick={false}
+        closeOnEscape={false}
+      >
+        {(close) => (
+          <div className="modal">
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="header">{textHeader}</div>
+            <div className="content">{children}
+            </div>
+            <div className="actions">
+              <button
+                className="button"
+                onClick={() => {
+                  console.log("modal closed ");
+                  close();
+                }}
+              >
+                Cerrar
+              </button>
+              {extraButton}
+            </div>
+          </div>
+        )}
+      </Popup>
+    )
+
+}
